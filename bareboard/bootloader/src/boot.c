87144db400a07e00a8f47afcbf703aa726f50cda
luyang00 <393780933@qq.com>
2016-07-08 15:49:49 +0800

    fix

--- bareboard/bootloader/src/boot.c
+++ bareboard/bootloader/src/boot.c
@@ -1,9 +1,15 @@
-#include "Setup.h"
+#include "setup.h"
+
+extern void uart0_init(void);
+extern void nand_read(unsigned int addr, unsigned char *buf, unsigned int len);
+extern void puts(char *str);
+extern void puthex(unsigned int val);
+
+
 static struct tag *params;
-#define MACH_TYPE_S3C2440 362 //smdk2440
-void setup_start_tags()
+
+void setup_start_tag(void)
 {
-	
 	params = (struct tag *)0x30000100;
 
 	params->hdr.tag = ATAG_CORE;
@@ -15,65 +21,89 @@ void setup_start_tags()
 
 	params = tag_next (params);
 }
-void setup_memory_tags()
+
+void setup_memory_tags(void)
 {
 	params->hdr.tag = ATAG_MEM;
 	params->hdr.size = tag_size (tag_mem32);
-
+	
 	params->u.mem.start = 0x30000000;
-	params->u.mem.size =  64*1024*1024;//64M
-
+	params->u.mem.size  = 64*1024*1024;
+	
 	params = tag_next (params);
 }
-int strlen(char * str)
+
+int strlen(char *str)
 {
-	int i=0;
-	while(str[i])
+	int i = 0;
+	while (str[i])
 	{
 		i++;
 	}
 	return i;
 }
-void strcpy(char * dest,char * src)
+
+void strcpy(char *dest, char *src)
 {
-	while((*dest++=*src++)!='\0');
+	while ((*dest++ = *src++) != '\0');
 }
-void setup_commandline_tags(char * cmdline)
+
+void setup_commandline_tag(char *cmdline)
 {
-	int len=strlen(cmdline)+1;
-	params->hdr.tag = ATAG_CMDLINE;
-	params->hdr.size =(sizeof (struct tag_header) + len + 3) >> 2;
+	int len = strlen(cmdline) + 1;
+	
+	params->hdr.tag  = ATAG_CMDLINE;
+	params->hdr.size = (sizeof (struct tag_header) + len + 3) >> 2;
 
 	strcpy (params->u.cmdline.cmdline, cmdline);
 
 	params = tag_next (params);
 }
-void setup_end_tag()
+
+void setup_end_tag(void)
 {
 	params->hdr.tag = ATAG_NONE;
 	params->hdr.size = 0;
 }
+
+
 int main(void)
 {
-	void (*theKernel)(int zero,int arch,unsigned int params);
-	led_on();		
-	/*0.bootloader should init serial port, linux kernel don't do init job*/
+	void (*theKernel)(int zero, int arch, unsigned int params);
+	volatile unsigned int *p = (volatile unsigned int *)0x30008000;
+
+	/* 0. 帮内核设置串口: 内核启动的开始部分会从串口打印一些信息,但是内核一开始没有初始化串口 */
 	uart0_init();
-	/*1.read kernel from nand flash*/
-	puts("copy kernel form nand...\n\r");
-	nand_read(0x4a0000+64,0x30008000,0x400000);
-	/*2.set bootargs*/
-	puts("set boot params...\n\r");
-	setup_start_tags();
+	
+	/* 1. 从NAND FLASH里把内核读入内存 */
+	puts("Copy kernel from nand\n\r");
+	nand_read(0x60000+64, (unsigned char *)0x30008000, 0x200000);
+	puthex(0x1234ABCD);
+	puts("\n\r");
+	puthex(*p);
+	puts("\n\r");
+
+	/* 2. 设置参数 */
+	puts("Set boot params\n\r");
+	setup_start_tag();
 	setup_memory_tags();
-	setup_commandline_tags("noninitrd root=/dev/mtdblock3 init=/linuxrc console锛漝ev/ttySAC0");
+	setup_commandline_tag("noinitrd root=/dev/mtdblock3 init=/linuxrc console=ttySAC0");
 	setup_end_tag();
 
-	/*3.jump to run linux kernel*/
-	puts("boot linux kernel...\n\r");
-	theKernel = (void(*)(int,int,unsigned int))0x30008000;
-	theKernel(0,MACH_TYPE_S3C2440,0x30000100);
-	/*never run to here*/
-	puts("error...\n\r");
+	/* 3. 跳转执行 */
+	puts("Boot kernel\n\r");
+	theKernel = (void (*)(int, int, unsigned int))0x30008000;
+	theKernel(0, 362, 0x30000100);  
+	/* 
+	 *  mov r0, #0
+	 *  ldr r1, =362
+	 *  ldr r2, =0x30000100
+	 *  mov pc, #0x30008000 
+	 */
+
+	puts("Error!\n\r");
+	/* 如果一切正常, 不会执行到这里 */
+
 	return -1;
 }
+
--- bareboard/bootloader/src/init.c
+++ bareboard/bootloader/src/init.c
@@ -1,16 +1,17 @@
-/*nand */
-#define NFCONF *((volatile unsigned long*)0x4E000000)
-#define NFCONT *((volatile unsigned long*)0x4E000004)
-#define NFCMMD *((volatile unsigned char*)0x4E000008)
-#define NFADDR *((volatile unsigned char*)0x4E00000c)
-#define NFDATA *((volatile unsigned char*)0x4E000010)
-#define NFSTAT *((volatile unsigned long*)0x4E000020)
 
-/*UART GPIO*/
+/* NAND FLASH控制器 */
+#define NFCONF (*((volatile unsigned long *)0x4E000000))
+#define NFCONT (*((volatile unsigned long *)0x4E000004))
+#define NFCMMD (*((volatile unsigned char *)0x4E000008))
+#define NFADDR (*((volatile unsigned char *)0x4E00000C))
+#define NFDATA (*((volatile unsigned char *)0x4E000010))
+#define NFSTAT (*((volatile unsigned char *)0x4E000020))
+
+/* GPIO */
 #define GPHCON              (*(volatile unsigned long *)0x56000070)
-#define GPHDAT              (*(volatile unsigned long *)0x56000074)
 #define GPHUP               (*(volatile unsigned long *)0x56000078)
-/*UART registers*/
+
+/* UART registers*/
 #define ULCON0              (*(volatile unsigned long *)0x50000000)
 #define UCON0               (*(volatile unsigned long *)0x50000004)
 #define UFCON0              (*(volatile unsigned long *)0x50000008)
@@ -20,20 +21,160 @@
 #define URXH0               (*(volatile unsigned char *)0x50000024)
 #define UBRDIV0             (*(volatile unsigned long *)0x50000028)
 
-/*LED*/
-#define GPFCON      (*(volatile unsigned long *)0x56000050)
-#define GPFDAT      (*(volatile unsigned long *)0x56000054)
-
-
 #define TXD0READY   (1<<2)
-#define RXD0READY   (1)
+
+
+void nand_read(unsigned int addr, unsigned char *buf, unsigned int len);
+
+
+int isBootFromNorFlash(void)
+{
+	volatile int *p = (volatile int *)0;
+	int val;
+
+	val = *p;
+	*p = 0x12345678;
+	if (*p == 0x12345678)
+	{
+		/* 写成功, 是nand启动 */
+		*p = val;
+		return 0;
+	}
+	else
+	{
+		/* NOR不能像内存一样写 */
+		return 1;
+	}
+}
+
+void copy_code_to_sdram(unsigned char *src, unsigned char *dest, unsigned int len)
+{	
+	int i = 0;
+	
+	/* 如果是NOR启动 */
+	if (isBootFromNorFlash())
+	{
+		while (i < len)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	else
+	{
+		//nand_init();
+		nand_read((unsigned int)src, dest, len);
+	}
+}
+
+void clear_bss(void)
+{
+	extern int __bss_start, __bss_end;
+	int *p = &__bss_start;
+	
+	for (; p < &__bss_end; p++)
+		*p = 0;
+}
+
+void nand_init(void)
+{
+#define TACLS   0
+#define TWRPH0  1
+#define TWRPH1  0
+	/* 设置时序 */
+	NFCONF = (TACLS<<12)|(TWRPH0<<8)|(TWRPH1<<4);
+	/* 使能NAND Flash控制器, 初始化ECC, 禁止片选 */
+	NFCONT = (1<<4)|(1<<1)|(1<<0);	
+}
+
+void nand_select(void)
+{
+	NFCONT &= ~(1<<1);	
+}
+
+void nand_deselect(void)
+{
+	NFCONT |= (1<<1);	
+}
+
+void nand_cmd(unsigned char cmd)
+{
+	volatile int i;
+	NFCMMD = cmd;
+	for (i = 0; i < 10; i++);
+}
+
+void nand_addr(unsigned int addr)
+{
+	unsigned int col  = addr % 2048;
+	unsigned int page = addr / 2048;
+	volatile int i;
+
+	NFADDR = col & 0xff;
+	for (i = 0; i < 10; i++);
+	NFADDR = (col >> 8) & 0xff;
+	for (i = 0; i < 10; i++);
+	
+	NFADDR  = page & 0xff;
+	for (i = 0; i < 10; i++);
+	NFADDR  = (page >> 8) & 0xff;
+	for (i = 0; i < 10; i++);
+	NFADDR  = (page >> 16) & 0xff;
+	for (i = 0; i < 10; i++);	
+}
+
+void nand_wait_ready(void)
+{
+	while (!(NFSTAT & 1));
+}
+
+unsigned char nand_data(void)
+{
+	return NFDATA;
+}
+
+void nand_read(unsigned int addr, unsigned char *buf, unsigned int len)
+{
+	int col = addr % 2048;
+	int i = 0;
+		
+	/* 1. 选中 */
+	nand_select();
+
+	while (i < len)
+	{
+		/* 2. 发出读命令00h */
+		nand_cmd(0x00);
+
+		/* 3. 发出地址(分5步发出) */
+		nand_addr(addr);
+
+		/* 4. 发出读命令30h */
+		nand_cmd(0x30);
+
+		/* 5. 判断状态 */
+		nand_wait_ready();
+
+		/* 6. 读数据 */
+		for (; (col < 2048) && (i < len); col++)
+		{
+			buf[i] = nand_data();
+			i++;
+			addr++;
+		}
+		
+		col = 0;
+	}
+
+	/* 7. 取消选中 */		
+	nand_deselect();
+}
 
 #define PCLK            50000000    // init.c中的clock_init函数设置PCLK为50MHz
 #define UART_CLK        PCLK        //  UART0的时钟源设为PCLK
 #define UART_BAUD_RATE  115200      // 波特率
 #define UART_BRD        ((UART_CLK  / (UART_BAUD_RATE * 16)) - 1)
-void nand_init(void);
-void nand_read(unsigned int addr,unsigned char *buf,unsigned int len);
+
 /*
  * 初始化UART0
  * 115200,8N1,无流控
@@ -49,6 +190,7 @@ void uart0_init(void)
     UMCON0  = 0x00;     // 不使用流控
     UBRDIV0 = UART_BRD; // 波特率为115200
 }
+
 /*
  * 发送一个字符
  */
@@ -60,141 +202,34 @@ void putc(unsigned char c)
     /* 向UTXH0寄存器中写入数据，UART即自动将它发送出去 */
     UTXH0 = c;
 }
+
 void puts(char *str)
 {
-	int i=0;
-	while(str[i])
+	int i = 0;
+	while (str[i])
 	{
-		putc((unsigned char)str[i]);
+		putc(str[i]);
 		i++;
 	}
 }
 
-void led_on()
+void puthex(unsigned int val)
 {
-    GPFCON = 0x00000100;    // 设置GPF4为输出口, 位[8:7]=0b01
-    GPFDAT = 0x00000000;    // GPF4输出0，LED1点亮
-}
-
-
-int isBootFromNorFlash(void)
-{
-	volatile int *p =(volatile int *)0;
-	int val;
-	val = *p;
-	*p = 0x12345678;
-	if(*p == 0x12345678)
-	{
-		/* write success nand start*/
-		*p=val;
-		return 0;
-	}
-	else
-	{
-		/*nor can't write */
-		return 1;
-	}
+	/* 0x1234abcd */
+	int i;
+	int j;
 	
+	puts("0x");
+
+	for (i = 0; i < 8; i++)
+	{
+		j = (val >> ((7-i)*4)) & 0xf;
+		if ((j >= 0) && (j <= 9))
+			putc('0' + j);
+		else
+			putc('A' + j - 0xa);
 		
-}
-void copy_code_to_sdram(unsigned char *src,unsigned char *dest,unsigned int length)
-{
-	int i=0;
-	/*如果是NOR启动*/
-	if(isBootFromNorFlash())
-	{
-		while(i<length)
-		{
-			*dest=*src;
-			i++;
-		}
 	}
-	else 
-	{
-		nand_read((unsigned int)src,dest,length);
-	}
-}
-void clean_bss(void)
-{
-	extern int __bss_start,__bss_end;
-	int *p = & __bss_start;
-	for(;p<&__bss_end;p++)
-		*p=0;
-}
-
-void nand_init(void)
-{
-	#define TACLS 	0
-	#define TWRPH0  1
-	#define TWRPH1 	0
-
-	/* 设置时序 */
-	NFCONF = (TACLS<<12)|(TWRPH0<<8)|(TWRPH1<<4);
-	/* 使能NAND Flash控制器, 初始化ECC, 禁止片选 */
-	NFCONT = (1<<4)|(1<<1)|(1<<0);	
-}
-void nand_cmd(unsigned char cmd)
-{
-	NFCMMD=cmd;
-}
-void nand_address(unsigned int addr)
-{
-	unsigned int col =addr/2048;
-	unsigned int page =addr%2048;
-	volatile  int i;
-	NFADDR = col & 0xff;
-	for(i=0;i<10;i++);
-	NFADDR=(col>>8)&0xff;
-	for(i=0;i<10;i++);
-	NFADDR= page &0xff;
-	for(i=0;i<10;i++);
-	NFADDR=(page>>8)&0xff;
-	for(i=0;i<10;i++);
 	
 }
-void nand_select(void)
-{
-	NFCONT&=~(1<<1);
-}
-void nand_deselect(void)
-{
-	NFCONT&=~(0<<1);
-}
 
-void nand_wait_ready()
-{
-	while(!NFSTAT&0x01);
-}
-unsigned char nand_data(void)
-{
-	return NFDATA;
-}
-	
-void nand_read(unsigned int addr,unsigned char *buf,unsigned int len)
-{
-int i=0;
-while (i<len)
-{
-	int col=addr/2048;
-	/*1.select(CE)*/
-	nand_select();
-	/*2.send write command*/
-	nand_cmd(0x00);
-	/*2.send column address*/
-	nand_cmd(addr);
-	/*3.send read command*/
-	nand_cmd(0x30);
-	/*4.wait for asynchoronous*/
-	nand_wait_ready();
-	/*5.read data*/		
-	for(;(col<2048)&&i<(len);col++)
-	{	
-		buf[i]=nand_data();
-		i++;
-		addr++;
-	}
-	col=0;
-	/*cancel select(CE)*/
-	nand_deselect();
-}
-}
